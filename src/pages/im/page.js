0984a3a227981ff3af9e195e5545b4a5c42de327
13045196846 <64435716@qq.com>
2018-04-12 16:18:21 +0800

    add im style file im.less

--- src/pages/im/page.js
+++ src/pages/im/page.js
@@ -1,9 +1,16 @@
 import InboxTable from './inbox/inbox-table';
+import './im.less';
 
 export default () => {
     return (
-        <div>
-           <InboxTable/>
+        <div className = 'im-page flex-horiz'>
+            <div className = 'im-aside flex-vert'>
+                <InboxTable/>
+            </div>
+            <div className = 'im-main flex-space felx-vert'>
+                
+            </div>
+           
         </div>
     )
 }
