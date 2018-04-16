8cc5a59bbe4405364ab22d5fac95e92948a5ab57
13045196846 <64435716@qq.com>
2018-04-16 16:08:33 +0800

    add initial messageeditor view

--- src/pages/im/channel/message/message-editor.js
+++ src/pages/im/channel/message/message-editor.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Textarea from 'react-textarea-autosize';
+
 import './message-editor.less'
 
 class MessageEditor extends React.Component {
     render () {
         return (
             <div className = 'message-editor flex-static'>
-                    this is editor
+                  <Textarea className = 'lite-textbox'/>
             </div>
         )
     }
