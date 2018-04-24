43345478b4bbe7bbe88e9677201550700f6c0cd8
13045196846 <64435716@qq.com>
2018-04-24 11:20:05 +0800

    add mock object

---
+++ mock/notifications.js
@@ -0,0 +1,418 @@
+import Mock from 'mockjs';
+
+let db = Mock.mock(  
+  [
+    {
+        "_id": "5acb1ffd5b2ed8983b93c70c", 
+        "type": "dms", 
+        "target": {
+            "_id": "5acb1da2384e3c007b0637e0", 
+            "name": "刘好义", 
+            "py": "lhy", 
+            "pinyin": "liuhaoyi", 
+            "emailForLogin": "25436136@qq.com", 
+            "emailDomain": "qq.com", 
+            "accountId": "5acb1da27c8c81787e7bb201", 
+            "__v": 2, 
+            "unions": [
+                {
+                    "_id": "5acec5ab2aeae2727ee1c718", 
+                    "avatarUrl": "https://avatars2.githubusercontent.com/u/37434399?v=4", 
+                    "name": "liuhaoyi", 
+                    "openId": "37434399", 
+                    "refer": "github"
+                }
+            ], 
+            "updatedAt": "2018-04-23T07:21:43.228Z", 
+            "createdAt": "2018-04-09T08:00:34.873Z", 
+            "isGuest": false, 
+            "isRobot": false, 
+            "pys": [
+                "lhy"
+            ], 
+            "pinyins": [
+                "liuhaoyi"
+            ], 
+            "from": "register", 
+            "avatarUrl": "https://striker.teambition.net/thumbnail/1113b798c50732d54a833cc5cc2f47e6145c/w/200/h/200", 
+            "id": "5acb1da2384e3c007b0637e0", 
+            "email": "25436136@qq.com"
+        }, 
+        "user": "572c94809e3c72ee38daf950", 
+        "_emitterId": "5add899d1f31e6b71308c6c8", 
+        "creator": {
+            "_id": "5acb1da2384e3c007b0637e0", 
+            "name": "刘好义", 
+            "py": "lhy", 
+            "pinyin": "liuhaoyi", 
+            "emailForLogin": "25436136@qq.com", 
+            "emailDomain": "qq.com", 
+            "accountId": "5acb1da27c8c81787e7bb201", 
+            "__v": 2, 
+            "unions": [
+                {
+                    "_id": "5acec5ab2aeae2727ee1c718", 
+                    "avatarUrl": "https://avatars2.githubusercontent.com/u/37434399?v=4", 
+                    "name": "liuhaoyi", 
+                    "openId": "37434399", 
+                    "refer": "github"
+                }
+            ], 
+            "updatedAt": "2018-04-23T07:21:43.228Z", 
+            "createdAt": "2018-04-09T08:00:34.873Z", 
+            "isGuest": false, 
+            "isRobot": false, 
+            "pys": [
+                "lhy"
+            ], 
+            "pinyins": [
+                "liuhaoyi"
+            ], 
+            "from": "register", 
+            "avatarUrl": "https://striker.teambition.net/thumbnail/1113b798c50732d54a833cc5cc2f47e6145c/w/200/h/200", 
+            "id": "5acb1da2384e3c007b0637e0", 
+            "email": "25436136@qq.com"
+        }, 
+        "team": "572c9493f7e5c8d9475286a5", 
+        "__v": 0, 
+        "_latestReadMessageId": "5add899d1f31e6b71308c6c8", 
+        "pinnedAt": "2018-04-10T07:15:38.825Z", 
+        "updatedAt": "2018-04-23T07:22:05.280Z", 
+        "createdAt": "2018-04-09T08:10:37.964Z", 
+        "isHidden": false, 
+        "isMute": false, 
+        "isPinned": true, 
+        "unreadNum": 0, 
+        "text": "最近有啥进展？？？", 
+        "_creatorId": "5acb1da2384e3c007b0637e0", 
+        "_targetId": "5acb1da2384e3c007b0637e0", 
+        "_teamId": "572c9493f7e5c8d9475286a5", 
+        "_userId": "572c94809e3c72ee38daf950", 
+        "id": "5acb1ffd5b2ed8983b93c70c"
+    }, 
+    {
+        "_id": "5aab86c17e29a99a0eeaa07b", 
+        "creator": {
+            "_id": "572c94809e3c72ee38daf950", 
+            "name": "王琤", 
+            "py": "wc", 
+            "pinyin": "wangcheng", 
+            "accountId": "572c947f45b5e81658178099", 
+            "__v": 2, 
+            "subAccountSid": "fae2cdd5138911e6bb9bac853d9f54f2", 
+            "unions": [
+                {
+                    "_id": "5acec1db2aeae2727ee1c717", 
+                    "avatarUrl": "https://avatars0.githubusercontent.com/u/38239481?v=4", 
+                    "name": "qteamcheng", 
+                    "openId": "38239481", 
+                    "refer": "github"
+                }
+            ], 
+            "updatedAt": "2018-04-24T02:43:28.466Z", 
+            "createdAt": "2016-05-06T12:56:32.366Z", 
+            "isGuest": false, 
+            "isRobot": false, 
+            "pys": [
+                "wc"
+            ], 
+            "pinyins": [
+                "wangcheng"
+            ], 
+            "from": "register", 
+            "avatarUrl": "https://dn-st.qbox.me/user_default_avatars/20.png", 
+            "id": "572c94809e3c72ee38daf950"
+        }, 
+        "type": "room", 
+        "target": {
+            "_id": "5aab86c17e29a99a0eeaa078", 
+            "email": "qteam.recfeb9f09j@mail.jianliao.com", 
+            "topic": "QTeam", 
+            "py": "qteam", 
+            "pinyin": "qteam", 
+            "creator": "572c94809e3c72ee38daf950", 
+            "team": "572c9493f7e5c8d9475286a5", 
+            "__v": 0, 
+            "updatedAt": "2018-03-16T08:56:33.293Z", 
+            "createdAt": "2018-03-16T08:56:33.293Z", 
+            "memberCount": 2, 
+            "pys": [
+                "qteam"
+            ], 
+            "pinyins": [
+                "qteam"
+            ], 
+            "isGuestVisible": true, 
+            "color": "blue", 
+            "isPrivate": true, 
+            "isArchived": false, 
+            "isGeneral": false, 
+            "popRate": 6, 
+            "_creatorId": "572c94809e3c72ee38daf950", 
+            "_teamId": "572c9493f7e5c8d9475286a5", 
+            "id": "5aab86c17e29a99a0eeaa078"
+        }, 
+        "user": "572c94809e3c72ee38daf950", 
+        "team": "572c9493f7e5c8d9475286a5", 
+        "__v": 0, 
+        "_emitterId": "5acf224f2536595d6f606786", 
+        "_latestReadMessageId": "5acf224f2536595d6f606786", 
+        "pinnedAt": "2018-04-17T08:22:39.919Z", 
+        "updatedAt": "2018-04-17T08:22:39.920Z", 
+        "createdAt": "2018-03-16T08:56:33.527Z", 
+        "isHidden": false, 
+        "isMute": false, 
+        "isPinned": true, 
+        "unreadNum": 0, 
+        "text": "{{__info-remove-integration}} GitHub", 
+        "_creatorId": "572c94809e3c72ee38daf950", 
+        "_targetId": "5aab86c17e29a99a0eeaa078", 
+        "_teamId": "572c9493f7e5c8d9475286a5", 
+        "_userId": "572c94809e3c72ee38daf950", 
+        "id": "5aab86c17e29a99a0eeaa07b"
+    }, 
+    {
+        "_id": "572ca131f7e5c8d947528ee2", 
+        "creator": {
+            "_id": "572c94809e3c72ee38daf950", 
+            "name": "王琤", 
+            "py": "wc", 
+            "pinyin": "wangcheng", 
+            "accountId": "572c947f45b5e81658178099", 
+            "__v": 2, 
+            "subAccountSid": "fae2cdd5138911e6bb9bac853d9f54f2", 
+            "unions": [
+                {
+                    "_id": "5acec1db2aeae2727ee1c717", 
+                    "avatarUrl": "https://avatars0.githubusercontent.com/u/38239481?v=4", 
+                    "name": "qteamcheng", 
+                    "openId": "38239481", 
+                    "refer": "github"
+                }
+            ], 
+            "updatedAt": "2018-04-24T02:43:28.466Z", 
+            "createdAt": "2016-05-06T12:56:32.366Z", 
+            "isGuest": false, 
+            "isRobot": false, 
+            "pys": [
+                "wc"
+            ], 
+            "pinyins": [
+                "wangcheng"
+            ], 
+            "from": "register", 
+            "avatarUrl": "https://dn-st.qbox.me/user_default_avatars/20.png", 
+            "id": "572c94809e3c72ee38daf950"
+        }, 
+        "type": "dms", 
+        "target": {
+            "_id": "547e7bbc496a2a3262a2bd51", 
+            "email": "talkai@talk.ai", 
+            "name": "Talkai", 
+            "pinyin": "talkai", 
+            "__v": 1, 
+            "globalRole": "user", 
+            "emailDomain": "talk.ai", 
+            "py": "talkai", 
+            "service": "talkai", 
+            "unions": [ ], 
+            "updatedAt": "2018-04-21T14:06:19.297Z", 
+            "createdAt": "2014-12-03T02:55:56.440Z", 
+            "isGuest": false, 
+            "isRobot": true, 
+            "pys": [
+                "talkai"
+            ], 
+            "pinyins": [
+                "talkai"
+            ], 
+            "from": "register", 
+            "avatarUrl": "https://dn-talk.oss.aliyuncs.com/images/icons/talkai@2x.png", 
+            "id": "547e7bbc496a2a3262a2bd51"
+        }, 
+        "user": "572c94809e3c72ee38daf950", 
+        "team": "572c9493f7e5c8d9475286a5", 
+        "__v": 0, 
+        "_emitterId": "59df6941144c49fb6f9c595e", 
+        "_latestReadMessageId": "59df6941144c49fb6f9c595e", 
+        "pinnedAt": "2018-03-21T02:20:01.146Z", 
+        "updatedAt": "2018-03-21T02:20:01.146Z", 
+        "createdAt": "2016-05-06T13:50:41.470Z", 
+        "isHidden": false, 
+        "isMute": true, 
+        "isPinned": true, 
+        "unreadNum": 0, 
+        "text": "{{__info-remove-message}}", 
+        "_creatorId": "572c94809e3c72ee38daf950", 
+        "_targetId": "547e7bbc496a2a3262a2bd51", 
+        "_teamId": "572c9493f7e5c8d9475286a5", 
+        "_userId": "572c94809e3c72ee38daf950", 
+        "id": "572ca131f7e5c8d947528ee2"
+    }, 
+    {
+        "_id": "5ab8b378bcaba7f00e14f69d", 
+        "creator": {
+            "_id": "572c94809e3c72ee38daf950", 
+            "name": "王琤", 
+            "py": "wc", 
+            "pinyin": "wangcheng", 
+            "accountId": "572c947f45b5e81658178099", 
+            "__v": 2, 
+            "subAccountSid": "fae2cdd5138911e6bb9bac853d9f54f2", 
+            "unions": [
+                {
+                    "_id": "5acec1db2aeae2727ee1c717", 
+                    "avatarUrl": "https://avatars0.githubusercontent.com/u/38239481?v=4", 
+                    "name": "qteamcheng", 
+                    "openId": "38239481", 
+                    "refer": "github"
+                }
+            ], 
+            "updatedAt": "2018-04-24T02:43:28.466Z", 
+            "createdAt": "2016-05-06T12:56:32.366Z", 
+            "isGuest": false, 
+            "isRobot": false, 
+            "pys": [
+                "wc"
+            ], 
+            "pinyins": [
+                "wangcheng"
+            ], 
+            "from": "register", 
+            "avatarUrl": "https://dn-st.qbox.me/user_default_avatars/20.png", 
+            "id": "572c94809e3c72ee38daf950"
+        }, 
+        "type": "dms", 
+        "target": {
+            "_id": "5ab8ac643fda301f0f3db38a", 
+            "name": "思恩", 
+            "py": "se", 
+            "pinyin": "sien", 
+            "emailForLogin": "wangcheng@isphere.top", 
+            "emailDomain": "isphere.top", 
+            "accountId": "5ab8ac63c97a47bf234eb705", 
+            "__v": 1, 
+            "unions": [ ], 
+            "updatedAt": "2018-04-12T08:38:58.162Z", 
+            "createdAt": "2018-03-26T08:16:36.481Z", 
+            "isGuest": false, 
+            "isRobot": false, 
+            "pys": [
+                "se"
+            ], 
+            "pinyins": [
+                "sien", 
+                "saien"
+            ], 
+            "from": "register", 
+            "avatarUrl": "https://dn-st.qbox.me/user_default_avatars/16.png", 
+            "id": "5ab8ac643fda301f0f3db38a", 
+            "email": "wangcheng@isphere.top"
+        }, 
+        "user": "572c94809e3c72ee38daf950", 
+        "team": "572c9493f7e5c8d9475286a5", 
+        "__v": 0, 
+        "_emitterId": "5ac4335474b192a57b4183c2", 
+        "_latestReadMessageId": "5ac4335474b192a57b4183c2", 
+        "updatedAt": "2018-04-16T07:27:11.185Z", 
+        "createdAt": "2018-03-26T08:46:48.707Z", 
+        "isHidden": false, 
+        "isMute": false, 
+        "isPinned": false, 
+        "unreadNum": 0, 
+        "text": "{{__info-remove-message}}", 
+        "_creatorId": "572c94809e3c72ee38daf950", 
+        "_targetId": "5ab8ac643fda301f0f3db38a", 
+        "_teamId": "572c9493f7e5c8d9475286a5", 
+        "_userId": "572c94809e3c72ee38daf950", 
+        "id": "5ab8b378bcaba7f00e14f69d"
+    }, 
+    {
+        "_id": "572c94939e3c72ee38daf957", 
+        "creator": {
+            "_id": "572c94809e3c72ee38daf950", 
+            "name": "王琤", 
+            "py": "wc", 
+            "pinyin": "wangcheng", 
+            "accountId": "572c947f45b5e81658178099", 
+            "__v": 2, 
+            "subAccountSid": "fae2cdd5138911e6bb9bac853d9f54f2", 
+            "unions": [
+                {
+                    "_id": "5acec1db2aeae2727ee1c717", 
+                    "avatarUrl": "https://avatars0.githubusercontent.com/u/38239481?v=4", 
+                    "name": "qteamcheng", 
+                    "openId": "38239481", 
+                    "refer": "github"
+                }
+            ], 
+            "updatedAt": "2018-04-24T02:43:28.466Z", 
+            "createdAt": "2016-05-06T12:56:32.366Z", 
+            "isGuest": false, 
+            "isRobot": false, 
+            "pys": [
+                "wc"
+            ], 
+            "pinyins": [
+                "wangcheng"
+            ], 
+            "from": "register", 
+            "avatarUrl": "https://dn-st.qbox.me/user_default_avatars/20.png", 
+            "id": "572c94809e3c72ee38daf950"
+        }, 
+        "type": "room", 
+        "target": {
+            "_id": "572c9493f7e5c8d9475286a6", 
+            "email": "general.r0026b490dk@mail.jianliao.com", 
+            "team": "572c9493f7e5c8d9475286a5", 
+            "topic": "general", 
+            "py": "general", 
+            "pinyin": "general", 
+            "creator": "572c94809e3c72ee38daf950", 
+            "__v": 0, 
+            "updatedAt": "2016-05-06T12:56:51.031Z", 
+            "createdAt": "2016-05-06T12:56:51.031Z", 
+            "memberCount": 4, 
+            "pys": [
+                "general"
+            ], 
+            "pinyins": [
+                "general"
+            ], 
+            "isGuestVisible": true, 
+            "color": "blue", 
+            "isPrivate": false, 
+            "isArchived": false, 
+            "isGeneral": true, 
+            "popRate": 12, 
+            "_creatorId": "572c94809e3c72ee38daf950", 
+            "_teamId": "572c9493f7e5c8d9475286a5", 
+            "id": "572c9493f7e5c8d9475286a6"
+        }, 
+        "user": "572c94809e3c72ee38daf950", 
+        "team": "572c9493f7e5c8d9475286a5", 
+        "__v": 0, 
+        "_emitterId": "5acf228505d9480a7cd8ab0e", 
+        "_latestReadMessageId": "5acf228505d9480a7cd8ab0e", 
+        "updatedAt": "2018-04-12T09:10:29.288Z", 
+        "createdAt": "2016-05-06T12:56:51.543Z", 
+        "isHidden": false, 
+        "isMute": false, 
+        "isPinned": false, 
+        "unreadNum": 0, 
+        "text": "{{__info-remove-integration}} Buildkite", 
+        "_creatorId": "572c94809e3c72ee38daf950", 
+        "_targetId": "572c9493f7e5c8d9475286a6", 
+        "_teamId": "572c9493f7e5c8d9475286a5", 
+        "_userId": "572c94809e3c72ee38daf950", 
+        "id": "572c94939e3c72ee38daf957"
+    }
+]
+)
+
+module.exports={
+    [`GET /api/notifications`](req,res){
+
+        res.status(200).json(db);
+    },
+}
