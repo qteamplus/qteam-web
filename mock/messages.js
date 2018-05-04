cef305be12257afcfbca55faaeae5456a06947a3
13045196846 <64435716@qq.com>
2018-05-04 09:14:33 +0800

    change message mock which can add _toId parameter

--- mock/messages.js
+++ mock/messages.js
@@ -1,5 +1,8 @@
 import Mock from 'mockjs';
 
+
+const qs = require('qs');
+
 let db = Mock.mock(
     [{
         "_id": "5ae90da41f31e6b7130ee188",
@@ -2306,11 +2309,19 @@ let db = Mock.mock(
         "_creatorId": "5acb1da2384e3c007b0637e0",
         "id": "5acdb5582536595d6f601130"
     }]
-)
+);
 
+
+let db1 = Mock.mock (
+    [{"_id":"5ae3cff71f31e6b7130e66de","to":{"_id":"5ab8ac643fda301f0f3db38a","name":"思恩","py":"se","pinyin":"sien","emailForLogin":"wangcheng@isphere.top","emailDomain":"isphere.top","accountId":"5ab8ac63c97a47bf234eb705","__v":1,"unions":[],"updatedAt":"2018-04-12T08:38:58.162Z","createdAt":"2018-03-26T08:16:36.481Z","isGuest":false,"isRobot":false,"pys":["se"],"pinyins":["sien","saien"],"from":"register","avatarUrl":"https://dn-st.qbox.me/user_default_avatars/16.png","id":"5ab8ac643fda301f0f3db38a","email":"wangcheng@isphere.top"},"team":"572c9493f7e5c8d9475286a5","creator":{"_id":"572c94809e3c72ee38daf950","name":"王琤","py":"wc","pinyin":"wangcheng","accountId":"572c947f45b5e81658178099","__v":2,"subAccountSid":"fae2cdd5138911e6bb9bac853d9f54f2","unions":[{"_id":"5acec1db2aeae2727ee1c717","avatarUrl":"https://avatars0.githubusercontent.com/u/38239481?v=4","name":"qteamcheng","openId":"38239481","refer":"github"}],"updatedAt":"2018-05-04T01:08:40.312Z","createdAt":"2016-05-06T12:56:32.366Z","isGuest":false,"isRobot":false,"pys":["wc"],"pinyins":["wangcheng"],"from":"register","avatarUrl":"https://dn-st.qbox.me/user_default_avatars/20.png","id":"572c94809e3c72ee38daf950"},"body":"hello， are you there ?","__v":0,"receiptors":[],"hasTag":false,"tags":[],"urls":[],"updatedAt":"2018-04-28T01:35:51.480Z","createdAt":"2018-04-28T01:35:51.480Z","displayType":"markdown","icon":"normal","isSystem":false,"attachments":[],"mentions":[],"type":"dms","_targetId":"5ab8ac643fda301f0f3db38a","_teamId":"572c9493f7e5c8d9475286a5","_toId":"5ab8ac643fda301f0f3db38a","_creatorId":"572c94809e3c72ee38daf950","id":"5ae3cff71f31e6b7130e66de"}]
+);
 module.exports={
     [`GET /api/messages`](req,res){
-
-        res.status(200).json(db);
+        const {_toId} = qs.parse(req.query);
+        if (_toId === '5acb1da2384e3c007b0637e0'){
+              res.status(200).json(db);
+        }else {
+            res.status(200).json(db1);
+        }
     },
 }
